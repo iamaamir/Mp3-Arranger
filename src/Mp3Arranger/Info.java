b60884a3f5df0007275a400fa76bdfefa7aa247f
Aamir khan <Aamir khan@192.168.0.17>
2016-07-25 16:10:58 +0530

    removed statistic setters and getters

--- src/Mp3Arranger/Info.java
+++ src/Mp3Arranger/Info.java
@@ -12,48 +12,20 @@ public class Info {
     private static File[] mp3;
     private static String TruePath;
     private static File fileSource;
-    private  int statis;
 
-    public static void setFileSource(File fileSource) {
-        Info.fileSource = fileSource;
-    }
+    public static void setFileSource(File fileSource) {Info.fileSource = fileSource;}
 
-    public static File getFileSource() {
-        return fileSource;
-    }
+    public static File getFileSource() {return fileSource;}
 
-    public static void setSortBy(String sortBy) {
-        Info.sortBy = sortBy;
-    }
+    public static void setSortBy(String sortBy) {Info.sortBy = sortBy;}
 
-    public static String getSortBy() {
-        return Info.sortBy;
-    }
+    public static String getSortBy() {return Info.sortBy;}
 
-    public static void setMp3(File[] mp3) {
+    public static void setMp3(File[] mp3) {Info.mp3 = mp3;}
 
-        Info.mp3 = mp3;
+    public static File[] getMp3() {return mp3;}
 
-    }
+    public static String getPath() {return TruePath;}
 
-    public static File[] getMp3() {
-        return mp3;
-    }
-
-    public static String getTruePath() {
-        return TruePath;
-    }
-
-    public static void setTruePath(String TruePath) {
-        Info.TruePath = TruePath;
-    }
-
-    public  void setStatis(int statis) {
-        this.statis = statis;
-    }
-
-    public  int getStatis() {
-        return statis;
-    }
-
+    public static void setPath(String TruePath) {Info.TruePath = TruePath;}
 }
