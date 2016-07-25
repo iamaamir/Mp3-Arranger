f9b9cc0a077ff8a87b1fe4e58b03c28f5a24a65f
Aamir khan <Aamir khan@192.168.0.17>
2016-07-25 14:58:56 +0530

    removed unnecessary threads and moved to SwingWorker

--- src/Mp3Arranger/GUI.java
+++ src/Mp3Arranger/GUI.java
@@ -1,13 +1,19 @@
 package Mp3Arranger;
 
+
+import static Mp3Arranger.Actions.EMO_ICON;
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.UnsupportedTagException;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -37,41 +43,42 @@ public class GUI extends JPanel implements ActionListener {
         } catch (UnsupportedLookAndFeelException ex) {
             ex.printStackTrace();
         }
-        path = new JTextField(ResourceBundle.getBundle("Mp3Arranger/config/Bundle").getString("PATH"), 20);
+        path = new JTextField(System.getProperty("user.home") + File.separatorChar + "Music", 20);
+
         JPanel pane = new JPanel();
         pane.add(path);
         super.add(pane, BorderLayout.PAGE_START);
-        
+
         browse.setMnemonic('b');
         browse.addActionListener(this);
-        
+
         choice.setEditable(false);
         choice.addActionListener(this);
-        
+
         go.setMnemonic('g');
-        
+
         JPanel pane2 = new JPanel();
         pane2.add(browse);
         pane2.add(choice);
         pane2.add(go);
-        
+
         super.add(pane2, BorderLayout.CENTER);
-        
+
         go.addActionListener(this);
-        
+
         folder = new JFileChooser();
         folder.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         folder.setMultiSelectionEnabled(false);
-        
+
         wait.setVisible(false);
-        
+
         JPanel pane3 = new JPanel();
-        
+
         pane3.add(wait);
         creadit = new JLabel("Copyright to Aamir khan 2014");
         pane3.add(creadit);
         super.add(pane3, BorderLayout.SOUTH);
-        
+
         path.setEditable(false);
         Dimension pathPreferredSize = wait.getPreferredSize();
         pathPreferredSize.width = 250;
@@ -90,7 +97,7 @@ public class GUI extends JPanel implements ActionListener {
 
                 Info.setTruePath(source.getPath());
                 path.setText(source.getPath());
-                File mp3Files[] = Actions.getMp3Files(path.getText());
+                File mp3Files[] = Actions.findMp3Files(path.getText());
                 if (mp3Files.length != 0) {
                     Info.setMp3(mp3Files);
                 } else {
@@ -120,29 +127,14 @@ public class GUI extends JPanel implements ActionListener {
             } else if (Info.getSortBy() == null) {
                 JOptionPane.showMessageDialog(null, "Please Select a Sort to Proceed", "Error!", JOptionPane.INFORMATION_MESSAGE);
             } else {
-
-                Thread th = new Thread(new Runnable() {
-
-                    @Override
-                    public void run() {
-
-                        initWait();
-                        Actions.SortFiles();
-                        wait.setVisible(false);
-                        reEnableButtons();
-                    }
-                });
-
-                th.start();
-
-                System.out.println(th.getName());
-
+                disableButtons();
+                runTask();
+                reEnableButtons();
             }
         }
     }
 
     protected static void showGUI() {
-
         Toolkit tk = Toolkit.getDefaultToolkit();
         Dimension screen = tk.getScreenSize();
         int sheight = screen.height / 4;
@@ -162,46 +154,10 @@ public class GUI extends JPanel implements ActionListener {
         gui.setIconImage(icon.getImage());
     }
 
-    void initWait() {
-
+    void disableButtons() {
         go.setEnabled(false);
         browse.setEnabled(false);
         choice.setEnabled(false);
-        wait.setVisible(true);
-        wait.setMinimum(0);
-        wait.setMaximum(Info.getMp3().length);
-        wait.setStringPainted(true);
-
-        Thread runner = new Thread() {
-
-            @Override
-            public void run() {
-                wait.setIndeterminate(true);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException ex) {
-                    System.out.println(ex.getCause());
-                }
-                wait.setIndeterminate(false);
-
-                while (true) {
-                    Runnable runme = new Runnable() {
-                        @Override
-                        public void run() {
-                            wait.setValue(Info.getStatis());
-                        }
-                    };
-                    SwingUtilities.invokeLater(runme);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException ex) {
-                    }
-
-                }
-
-            }
-        };
-        runner.start();
     }
 
     public static void main(String args[]) {
@@ -219,5 +175,100 @@ public class GUI extends JPanel implements ActionListener {
         go.setEnabled(true);
         browse.setEnabled(true);
         choice.setEnabled(true);
+    }
+
+    private void runTask() {
+        SwingWorker<Void, Void> task = new SwingWorker<Void, Void>() {
+            private String tag;
+            private File dirPath;
+            Actions dataHandler = new Actions();
+            int tracker;
+
+            @Override
+            protected Void doInBackground() throws Exception {
+                wait.setVisible(true);
+                wait.setMinimum(0);
+                wait.setMaximum(Info.getMp3().length);
+                wait.setStringPainted(true);
+
+                String SLASH = File.separator;
+
+                for (File mp3 : Info.getMp3()) {
+                    try {
+                        Mp3File song = new Mp3File(mp3.getAbsolutePath());
+
+                        if (song.hasId3v2Tag()) {
+                            ID3v2 idv2 = song.getId3v2Tag();
+                            switch (Info.getSortBy()) {
+                                case "By Artist":
+                                    tag = idv2.getArtist();
+                                    tag = (tag == null) ? "Unknown Artist" : tag;
+                                    dirPath = new File(Info.getTruePath() + SLASH + tag);
+                                    if (!dirPath.exists()) {
+                                        dirPath.mkdirs();
+
+                                    }
+                                    tracker = dataHandler.CopyData(mp3.getAbsolutePath(), dirPath.toString());
+                                    wait.setValue(tracker);
+                                    break;
+                                case "By Album":
+                                    tag = idv2.getAlbum();
+                                    tag = (tag == null) ? "Unknown Album" : tag;
+                                    dirPath = new File(Info.getTruePath() + SLASH + tag);
+                                    if (!dirPath.exists()) {
+                                        dirPath.mkdirs();
+
+                                    }
+                                    tracker = dataHandler.CopyData(mp3.getAbsolutePath(), dirPath.toString());
+                                    wait.setValue(tracker);
+                                    break;
+                                case "By Genre":
+                                    tag = idv2.getGenreDescription();
+                                    tag = (tag == null) ? "Unknown Genere" : tag;
+                                    dirPath = new File(Info.getTruePath() + SLASH + tag);
+                                    if (!dirPath.exists()) {
+                                        dirPath.mkdirs();
+
+                                    }
+                                    tracker = dataHandler.CopyData(mp3.getAbsolutePath(), dirPath.toString());
+                                    wait.setValue(tracker);
+                                    break;
+                                default:
+                                    System.err.println("Unknow Sort");
+                            }
+
+                        } else {
+                            dirPath = new File(Info.getTruePath() + SLASH + "Un-Defined Tag");
+                            if (!dirPath.exists()) {
+                                dirPath.mkdir();
+                            }
+                            tracker = dataHandler.CopyData(mp3.getAbsolutePath(), dirPath.toString());
+                            wait.setValue(tracker);
+                        }
+
+                    } catch (IOException | UnsupportedTagException | InvalidDataException ex) {
+                        JOptionPane.showMessageDialog(null, ex.getMessage());
+                    }
+
+                }
+
+                JLabel taskdoneMsg = new JLabel();
+                taskdoneMsg.setIcon(EMO_ICON);
+                taskdoneMsg.setText("<html><body><h3 style = color:Green;>Task Completed Successfully </h3></body></html>");
+
+                JOptionPane.showMessageDialog(null, taskdoneMsg, "Done", JOptionPane.PLAIN_MESSAGE);
+
+                return null;
+            }
+
+            @Override
+            protected void done() {
+                Info.setMp3(null);
+                wait.setVisible(false);
+            }
+
+        };
+
+        task.execute();
     }
 }
